3673f289eb7503ac5048ac4361289adccc87f267
MichaelMartin02 <michaelleemartin02@gmail.com>
2025-01-14 10:59:20 -0500

    Removal

--- js/cursor.js
+++
@@ -1,9 +0,0 @@
-const cursor = document.querySelector('.cursor');
-const moveCursor = (e)=> {
-  cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
-}
-
-window.addEventListener('mousemove', moveCursor)
-
-
-
